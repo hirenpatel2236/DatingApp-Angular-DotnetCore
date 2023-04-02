8bf37d1ddd4c97b3f5c07ee2552b89ba4c35d849
Hiren Patel <hirenpatel2236@gmail.com>
2023-04-02 13:47:26 +0530

    Api User auth

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
