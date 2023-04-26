855c2bb1df73245f051e5478c8ec82fd175d1129
Hiren Patel <hiren@bytescout.com>
2023-04-26 22:44:07 +0530

    Persistant Login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -9,19 +11,14 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {}
-  loggedIn = false;
-
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.loggedIn = true;
-      },
+      next: response => { },
       error: error => {
         console.log(error);
       }
@@ -29,7 +26,7 @@ export class NavComponent implements OnInit {
   }
 
   logout(){
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 
 }
