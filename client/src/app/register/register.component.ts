da416ad087a3363a0dadb61c12c30ec7b28acbdc
Hiren Patel <hiren@bytescout.com>
2023-04-29 15:55:12 +0530

    End of section 6. Routing related changes

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,32 +1,38 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
-  model:any = {}
+  model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  register(){
-    this.accountService.register(this.model)
-    .subscribe({
-      next: response => {this.cancel();},
-      error: err => console.log(err)
-    })
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: (response) => {
+        this.cancel();
+      },
+      error: (err) => {
+        console.log(err);
+        this.toastr.error(err.error);
+      },
+    });
     console.log(this.model);
   }
 
-  cancel(){
+  cancel() {
     this.cancelRegister.emit(false);
   }
-
 }
