64f915f7592199ca94319f9361673478e8728b34
Hiren Patel <hiren@bytescout.com>
2023-04-29 13:59:11 +0530

    added registration

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,9 +9,11 @@ import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [
     BrowserModule, 
     AppRoutingModule, 
