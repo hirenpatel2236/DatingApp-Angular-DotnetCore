a3fb48eb9bbe805478648526dd2e58a386003709
Hiren Patel <hiren@bytescout.com>
2023-05-05 21:17:04 +0530

    End of section 12 - Added DatePicker and ReactiveForm

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -11,60 +11,63 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
-  styleUrls: ['./photo-editor.component.css']
+  styleUrls: ['./photo-editor.component.css'],
 })
 export class PhotoEditorComponent implements OnInit {
-
   @Input() member: Member | undefined;
   uploader: FileUploader | undefined;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
   user: User | undefined;
 
-  
-  constructor(private accountService: AccountService, private memberService: MembersService) {
+  constructor(
+    private accountService: AccountService,
+    private memberService: MembersService
+  ) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
-      next: user => {
-        if(user) this.user = user;
-      }
-    })
-   }
+      next: (user) => {
+        if (user) this.user = user;
+      },
+    });
+  }
 
   ngOnInit(): void {
     this.initializeUploader();
   }
 
-  fileOverBase(e:any){
+  fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e;
   }
 
-  setMainPhoto(photo: Photo){
+  setMainPhoto(photo: Photo) {
     this.memberService.setMainPhoto(photo.id).subscribe({
-      next: _ => {
-        if(this.user && this.member){
+      next: (_) => {
+        if (this.user && this.member) {
           this.user.photoUrl = photo.url;
           this.accountService.setCurrentUser(this.user);
           this.member.photoUrl = photo.url;
-          this.member.photos.forEach( p => {
-            if(p.isMain) p.isMain = false;
-            if(p.id === photo.id) p.isMain = true;
-          })
+          this.member.photos.forEach((p) => {
+            if (p.isMain) p.isMain = false;
+            if (p.id === photo.id) p.isMain = true;
+          });
         }
-      }
-    })
+      },
+    });
   }
 
-  deletePhoto(photo: Photo){
+  deletePhoto(photo: Photo) {
     this.memberService.deletePhoto(photo.id).subscribe({
-      next: _ => {
-        if(this.member){
-          this.member.photos = this.member.photos.filter(x => x.id !== photo.id);
+      next: (_) => {
+        if (this.member) {
+          this.member.photos = this.member.photos.filter(
+            (x) => x.id !== photo.id
+          );
         }
-      }
-    })
+      },
+    });
   }
 
-  initializeUploader(){
+  initializeUploader() {
     this.uploader = new FileUploader({
       url: this.baseUrl + 'users/add-photo',
       authToken: 'Bearer ' + this.user?.token,
@@ -72,19 +75,25 @@ export class PhotoEditorComponent implements OnInit {
       allowedFileType: ['image'],
       removeAfterUpload: true,
       autoUpload: false,
-      maxFileSize: 10 * 1024 * 1024
+      maxFileSize: 10 * 1024 * 1024,
     });
 
     this.uploader.onAfterAddingFile = (file) => {
       file.withCredentials = false;
-    }
+    };
 
     this.uploader.onSuccessItem = (item, response, status, headers) => {
-      if(response){
+      if (response) {
         const photo = JSON.parse(response);
-        this.member?.photos.push(photo);
-      }
-    }
-  }
 
+        this.member?.photos.push(photo);
+
+        if (photo.isMain && this.user && this.member) {
+          this.user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+          this.member.photoUrl = photo.url;
+        }
+      }
+    };
+  }
 }
