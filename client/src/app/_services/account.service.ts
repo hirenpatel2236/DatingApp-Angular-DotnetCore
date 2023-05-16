6eeea8cda1b64acc8d981899a8792337dba324ae
Hiren Patel <hiren@bytescout.com>
2023-05-16 17:49:08 +0530

    End of Section 16 Added Identity

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -5,45 +5,52 @@ import { User } from '../_models/user';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  login(model: any){
-    return this.http.post<User>(this.baseUrl + 'account/login', model)
-    .pipe(
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user){
+        if (user) {
           this.setCurrentUser(user);
         }
       })
     );
   }
 
-  register(model: any){
-    return this.http.post<User>(this.baseUrl + 'account/register', model)
-    .pipe(
-      map(user => {
-        if(user){
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user) => {
+        if (user) {
           this.setCurrentUser(user);
         }
       })
     );
   }
 
-  setCurrentUser(user: User){
+  setCurrentUser(user: User) {
+    user.roles = [];
+    
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
+
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token: String) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
