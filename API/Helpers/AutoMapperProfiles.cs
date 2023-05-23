f648d39802c25645a5ec31ffce0704b27e21a91e
Hiren Patel <hirenpatel2236@gmail.com>
2023-05-23 20:36:58 +0530

    End of section 17 Added signalR for message communications

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -26,6 +26,13 @@ namespace API.Helpers
                     .FirstOrDefault(x => x.IsMain).Url))
             ;
 
+            CreateMap<DateTime, DateTime>()
+                .ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+
+            CreateMap<DateTime?, DateTime?>()
+                .ConvertUsing(d => d.HasValue 
+                    ? DateTime.SpecifyKind(d.Value, DateTimeKind.Utc)
+                    : null);
         }
     }
 }
